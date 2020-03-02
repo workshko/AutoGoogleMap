93b3f76426ff9f444e9d11c74b3235879c25432d
shko <work.shko@gmail.com>
2020-03-02 14:52:42 +0900

    SEO 최적화 소스 업로드

---
+++ AutoGoogleMap/mainForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace AutoGoogleMap
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.btnTest = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.txtGoogleID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGooglePW = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkChrome = new System.Windows.Forms.CheckBox();
+            this.txtFileDir = new System.Windows.Forms.TextBox();
+            this.btnFileSelect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(12, 271);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(105, 43);
+            this.btnTest.TabIndex = 7;
+            this.btnTest.Text = "작업시작";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.txtLog);
+            this.groupBox5.Location = new System.Drawing.Point(12, 341);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(599, 261);
+            this.groupBox5.TabIndex = 8;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "최근 실행 로그";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Location = new System.Drawing.Point(3, 17);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.txtLog.Size = new System.Drawing.Size(593, 241);
+            this.txtLog.TabIndex = 0;
+            this.txtLog.Text = "";
+            // 
+            // txtGoogleID
+            // 
+            this.txtGoogleID.Location = new System.Drawing.Point(57, 9);
+            this.txtGoogleID.Name = "txtGoogleID";
+            this.txtGoogleID.Size = new System.Drawing.Size(178, 21);
+            this.txtGoogleID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "구글ID";
+            // 
+            // txtGooglePW
+            // 
+            this.txtGooglePW.Location = new System.Drawing.Point(57, 36);
+            this.txtGooglePW.Name = "txtGooglePW";
+            this.txtGooglePW.Size = new System.Drawing.Size(178, 21);
+            this.txtGooglePW.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "암호";
+            // 
+            // chkChrome
+            // 
+            this.chkChrome.AutoSize = true;
+            this.chkChrome.Location = new System.Drawing.Point(479, 14);
+            this.chkChrome.Name = "chkChrome";
+            this.chkChrome.Size = new System.Drawing.Size(128, 16);
+            this.chkChrome.TabIndex = 3;
+            this.chkChrome.Text = "크롬창 숨겨서 실행";
+            this.chkChrome.UseVisualStyleBackColor = true;
+            // 
+            // txtFileDir
+            // 
+            this.txtFileDir.Location = new System.Drawing.Point(12, 75);
+            this.txtFileDir.Name = "txtFileDir";
+            this.txtFileDir.ReadOnly = true;
+            this.txtFileDir.Size = new System.Drawing.Size(514, 21);
+            this.txtFileDir.TabIndex = 4;
+            // 
+            // btnFileSelect
+            // 
+            this.btnFileSelect.Location = new System.Drawing.Point(532, 73);
+            this.btnFileSelect.Name = "btnFileSelect";
+            this.btnFileSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnFileSelect.TabIndex = 5;
+            this.btnFileSelect.Text = "파일선택";
+            this.btnFileSelect.UseVisualStyleBackColor = true;
+            this.btnFileSelect.Click += new System.EventHandler(this.btnFileSelect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(385, 72);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "※ 메모장 파일을 이용하세요. 구분자는 반드시 \'|\' 로 지정 개행문자\'^\'\r\n\r\n\r\n예시 ) 위치주소|제목|설명 \r\n\r\n         강남대로" +
+    "92길|앱기획|앱기획설명^www.naver.com";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 615);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnFileSelect);
+            this.Controls.Add(this.txtFileDir);
+            this.Controls.Add(this.chkChrome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGoogleID);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtGooglePW);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "검색엔진 최적화 작업";
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.TextBox txtGoogleID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGooglePW;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkChrome;
+        private System.Windows.Forms.TextBox txtFileDir;
+        private System.Windows.Forms.Button btnFileSelect;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AutoGoogleMap/mainForm.cs
@@ -0,0 +1,387 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoGoogleMap
+{
+    public partial class mainForm : Form
+    {
+        private delegate void SetTextCallback(string text); //로그기록
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void SetLog(string log)
+        {
+            if (this.txtLog.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetLog);
+                this.Invoke(d, new object[] { log });
+            }
+            else
+            {
+                //this.txtLog.Text += log;
+                this.txtLog.Focus();
+                this.txtLog.AppendText(log);
+            }
+        }
+
+        /// <summary>
+        /// 오류 발생시 다시 초기 셋팅으로 돌아오도록 함.
+        /// </summary>
+        /// <param name="driver"></param>
+        private void SetInit(IWebDriver driver)
+        {
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+
+            ReadOnlyCollection<string> windowHandles = driver.WindowHandles;
+
+            foreach (var handel in windowHandles)
+            {
+                if (handel == driver.CurrentWindowHandle)
+                    continue;
+
+                driver.Close();
+            }
+
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+
+            driver.Url = "https://www.google.co.kr/maps/@37.5839889,126.8613182,14z/data=!4m2!10m1!1e2";
+
+            Thread.Sleep(3000);
+
+            var pwTag = driver.FindElement(By.CssSelector("button[aria-label='지도']"));
+            pwTag.Click();
+        }
+
+        private void StartWork()
+        {
+            
+            try
+            {
+                var driverService = ChromeDriverService.CreateDefaultService();
+                driverService.HideCommandPromptWindow = true; //크롬 콘솔창 숨기기 
+
+                var options = new ChromeOptions();
+
+                if (this.chkChrome.Checked == true)
+                {
+                    options.AddArguments("disable-infobars");
+                    options.AddArguments("--js-flags=--expose-gc");
+                    options.AddArguments("--enable-precise-memory-info");
+                    options.AddArguments("--disable-popup-blocking");
+                    options.AddArguments("--disable-default-apps");
+                    options.AddArguments("--headless");
+                }
+
+                using (IWebDriver driver = new ChromeDriver(driverService, options))
+                {
+                    string id = this.txtGoogleID.Text.Trim();
+                    string pw = this.txtGooglePW.Text.Trim();
+
+                    SetLog("구글 로그인 시작....\n");
+
+                    driver.Url = "https://stackoverflow.com/users/login?ssrc=head";
+                    var googleBtn = driver.FindElement(By.ClassName("s-btn__google"));
+                    googleBtn.Click();
+                    Thread.Sleep(2000);
+
+                    var emailTag = driver.FindElement(By.Name("identifier"));
+                    emailTag.SendKeys(id);
+
+                    var nextBtn = driver.FindElement(By.Id("identifierNext"));
+                    nextBtn.Click();
+
+                    Thread.Sleep(5000);
+
+                    var passwordTag = driver.FindElement(By.Name("password"));
+                    passwordTag.SendKeys(pw);
+
+                    //암호 입력 버튼
+                    var passNextBtn = driver.FindElement(By.Id("passwordNext"));
+                    passNextBtn.Click();
+
+                    Thread.Sleep(5000);
+
+                    driver.Url = "https://www.google.co.kr/maps/@37.5839889,126.8613182,14z/data=!4m2!10m1!1e2";
+
+                    Thread.Sleep(5000);
+
+                    var login_div = driver.FindElement(By.Id("gb"));
+
+                    if (login_div.Text == "로그인")
+                    {
+                        SetLog("구글 로그인 실패....\n");
+                        return;
+                    }
+
+                    SetLog("구글 로그인 성공....\n");
+                    SetLog("구글맵 페이지 이동완료....\n");
+
+                    var pwTag = driver.FindElement(By.CssSelector("button[aria-label='지도']"));
+                    pwTag.Click();
+
+                    Thread.Sleep(1500);
+
+                    string[] txtLines = System.IO.File.ReadAllLines(this.txtFileDir.Text);
+
+                    //반복 작업 시작!
+                    foreach (string lineText in txtLines)
+                    {
+
+                        string strAddress = "";     //지도를 찍을 위치
+                        string strMapTitle = "";    //제목글
+                        string strMapRemark = "";   //설명글
+
+                        try
+                        {
+
+                            string[] keywords = lineText.Split('|');
+
+                            if (keywords.Length != 3)
+                                continue;
+
+                            strAddress = keywords[0];
+                            strMapTitle = keywords[1];
+                            strMapRemark = keywords[2];
+
+                            Thread.Sleep(1500);
+
+                            //지도 만들기
+                            var createBtn = driver.FindElement(By.CssSelector("span.section-my-maps-create-button"));
+                            createBtn.Click();
+
+                            Thread.Sleep(2500);
+
+                            System.Diagnostics.Debug.Print(driver.WindowHandles[0]);    //현재창
+
+                            //새창으로 전환
+                            driver.SwitchTo().Window(driver.WindowHandles[1]);
+
+                            Thread.Sleep(250);
+
+                            var mapTitle = driver.FindElement(By.CssSelector("div[aria-label='제목없는 지도']"));
+                            mapTitle.Click();
+
+                            Thread.Sleep(250);
+
+                            //update-map
+                            var modalPopup = driver.FindElement(By.Id("update-map"));
+
+                            var mapUpTitle = modalPopup.FindElement(By.CssSelector("input[type=text]"));
+                            var mapUpRemark = modalPopup.FindElement(By.CssSelector("textarea"));
+                            var mapUpSave = modalPopup.FindElement(By.Name("save"));
+
+                            Thread.Sleep(250);
+
+                            mapUpTitle.SendKeys(strMapTitle);
+
+                            foreach (string text in strMapRemark.Split('^'))
+                            {
+                                mapUpRemark.SendKeys(text);
+                                mapUpRemark.SendKeys(OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter);
+                            }
+
+                            mapUpSave.Click();
+
+                            Thread.Sleep(250);
+
+                            //지도 검색어
+                            var mapKeyword = driver.FindElement(By.Id("mapsprosearch-field"));
+                            var mapBtn = driver.FindElement(By.Id("mapsprosearch-button"));
+
+                            mapKeyword.SendKeys(strAddress);
+                            mapBtn.Click();
+
+                            Thread.Sleep(2000);
+
+                            var addMapBtn = driver.FindElement(By.Id("addtomap-button"));
+                            addMapBtn.Click();
+
+                            Thread.Sleep(500);
+
+                            var modifyMapBtn = driver.FindElement(By.Id("map-infowindow-edit-button"));
+                            modifyMapBtn.Click();
+
+                            // 찍힌 주소명 수정
+
+                            var mapInfoWindow = driver.FindElement(By.Id("map-infowindow-container"));
+
+                            var mapInfoTitle = mapInfoWindow.FindElement(By.Id("map-infowindow-attr-이름-value"));
+                            var mapInfoRemark = mapInfoWindow.FindElement(By.Id("map-infowindow-attr-설명-value"));
+
+
+                            mapInfoTitle.Clear();
+                            mapInfoTitle.SendKeys(strMapTitle);
+
+                            Thread.Sleep(250);
+
+                            foreach (string text in strMapRemark.Split('^'))
+                            {
+                                mapInfoRemark.SendKeys(text);
+                                mapInfoRemark.SendKeys(OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter);
+                            }
+
+                            mapInfoRemark.SendKeys(OpenQA.Selenium.Keys.Enter);
+
+                            //Clipboard.SetText(strMapRemark);
+                            //mapInfoRemark.SendKeys(OpenQA.Selenium.Keys.Control + "v");
+
+                            Thread.Sleep(500);
+
+                            //공유 시작
+
+                            var btnShare = driver.FindElement(By.Id("map-action-share"));
+                            btnShare.Click();
+
+                            Thread.Sleep(2500);
+
+                            //액세스 권한 변경(모든사용자)
+
+                            var shareModal = driver.FindElement(By.CssSelector("div[guidedhelpid='share_dialog']"));
+
+                            //iframe이 있어서 스위치시켜주어야한다.
+                            var shareModaliFrame = shareModal.FindElement(By.TagName("iframe"));
+
+                            driver.SwitchTo().Frame(shareModaliFrame);
+
+                            var modifyAccess = driver.FindElement(By.Id(":h.change-link"));
+
+                            modifyAccess.Click();
+
+                            //사용 - 모든 웹 사용자
+                            var shareCheck = driver.FindElement(By.Id(":27.pub"));
+                            shareCheck.Click();
+
+                            //링크공유 저장
+                            var shareSave = driver.FindElement(By.Id(":27.save"));
+                            shareSave.Click();
+
+                            Thread.Sleep(2500);
+
+                            //완료
+                            var shareComplete = driver.FindElement(By.Id(":1a.close"));
+                            shareComplete.Click();
+
+                            Thread.Sleep(2000);
+
+                            //현재 활성화 탭 닫기
+                            driver.Close();
+
+                            //첫번째 창으로 이동
+                            driver.SwitchTo().Window(driver.WindowHandles[0]);
+
+                            Thread.Sleep(1500);
+
+                            SetLog(string.Format("작업완료.......  키워드 : {0}, {1}, {2}\n", strMapTitle, strMapRemark, strAddress));
+                        }
+                        catch (Exception exception)
+                        {
+                            SetLog(string.Format("반복작업 중 오류발생 - 키워드 : {0}, {1}, {2}\n", strMapTitle, strMapRemark, strAddress));
+                            SetLog(exception.Message + "\n\n");
+
+                            SetInit(driver);
+                        }
+
+                    }
+
+                    SetLog("최적화 작업이 완료되었습니다.\n");
+                    SetLog("-----------------------------------------\n\n");
+
+                    this.Invoke(new Action(delegate()
+                    {
+                        this.txtFileDir.Text = string.Empty;
+                        this.btnTest.Enabled = true;
+                    }));
+
+                }
+            }
+            catch (Exception ee)
+            {
+                if (ee.Message.Contains("gb"))
+                {
+                    SetLog("오류내용 : ");
+                    SetLog("구글 아이디 또는 암호가 일치하지 않습니다.");
+                }
+
+                SetLog(ee.Message);
+
+                MessageBox.Show("작업 중 오류가 발생하였습니다..");
+
+            }
+
+        }
+
+        #region 이벤트
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            if (this.txtGoogleID.Text == String.Empty)
+            {
+                MessageBox.Show("구글 로그인 아이디를 입력하세요.");
+                this.txtGoogleID.Focus();
+                return;
+            }
+
+            if (this.txtGooglePW.Text == String.Empty)
+            {
+                MessageBox.Show("구글 암호를 입력하세요.");
+                this.txtGooglePW.Focus();
+                return;
+            }
+
+            if (this.txtFileDir.Text == String.Empty)
+            {
+                MessageBox.Show("작업하실 파일을 선택 하세요.");
+                return;
+            }
+
+            if (File.Exists(this.txtFileDir.Text) == false)
+            {
+                MessageBox.Show("작업경로에 파일이 존재하지 않습니다.");
+                return;
+            }
+            SetLog("-----------------------------------------\n\n");
+            SetLog("작업 시작....\n");
+
+            this.btnTest.Enabled = false;
+
+            Thread worker = new Thread(new ThreadStart(StartWork));
+            worker.Start();   
+        }
+
+        private void btnFileSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "작업파일 열기";
+            ofd.Filter = "txt 파일 (*.txt) | *.txt;";
+            ofd.Multiselect = false;
+
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.txtFileDir.Text = ofd.FileName;
+            }
+
+        }
+
+        #endregion
+
+        
+
+    }
+}
